2d0c75bddce56309047083e5104eb30a7cf40227
jnosis <jnosis1546@gmail.com>
2023-04-09 14:27:31 +0900

    refactor(server): Change gpt from fetch to module

--- server/src/openai.ts
+++ server/src/openai.ts
@@ -1,120 +1,23 @@
+import { OpenAI } from 'openai';
 import config from '~/config.ts';
 
 const apiKey = config.openai.apiKey;
 
-const baseUrl = 'https://api.openai.com/v1';
-
-type FeatureType = 'sentiment' | 'moderation';
-type Feature = {
-  url: string;
-  // deno-lint-ignore ban-types
-  data(text: string): Object;
-};
-type Features = Record<FeatureType, Feature>;
-
-const features: Features = {
-  sentiment: {
-    url: 'completions',
-    data: (text: string) => {
-      return {
-        model: 'text-davinci-003',
-        prompt:
-          `What is the sentiment of the following text?\n"${text}"\nSentiment:`,
-        max_tokens: 1,
-        temperature: 0,
-        stop: '\n',
-      };
-    },
-  },
-  moderation: {
-    url: 'moderations',
-    data: (text: string) => {
-      return { input: text };
-    },
-  },
-};
-
-async function f<Data>(
-  type: FeatureType,
-  text: string,
-): Promise<Data> {
-  const res = await fetch(`${baseUrl}/${features[type].url}`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`,
-    },
-    body: JSON.stringify(features[type].data(text)),
-  });
-
-  let data;
-
-  try {
-    if (res.status !== 204) data = await res.json();
-  } catch (error) {
-    throw new Error(error.message, { cause: res.status });
-  }
-
-  if (res.status > 299 || res.status < 200) {
-    const message = data && data.message
-      ? data.message
-      : 'Something went wrong';
-    throw new Error(message, { cause: res.status });
-  }
-
-  return data as Data;
-}
-
-type SentimentResponse = {
-  id: string;
-  object: string;
-  created: number;
-  model: string;
-  choices: {
-    text: string;
-    index: number;
-    logprobs: number | null;
-    finish_reason: string;
-  }[];
-  usage: {
-    prompt_tokens: number;
-    completion_tokens: number;
-    total_tokens: number;
-  };
-};
-
-type ModerationsResponse = {
-  id: string;
-  model: string;
-  results: {
-    flagged: boolean;
-    categories: {
-      sexual: boolean;
-      hate: boolean;
-      violence: boolean;
-      'self-harm': boolean;
-      'sexual/minors': boolean;
-      'hate/threatening': boolean;
-      'violence/graphic': boolean;
-    };
-    category_scores: {
-      sexual: number;
-      hate: number;
-      violence: number;
-      'self-harm': number;
-      'sexual/minors': number;
-      'hate/threatening': number;
-      'violence/graphic': number;
-    };
-  }[];
-};
+const client = new OpenAI(apiKey);
 
 const openai = {
   async classifySentiment(text: string) {
-    return await f<SentimentResponse>('sentiment', text);
+    return await client.createCompletion({
+      model: 'davinci',
+      prompt:
+        `What is the sentiment of the following text?\n"${text}"\nSentiment:`,
+      maxTokens: 1,
+      temperature: 0,
+      stop: '\n',
+    });
   },
-  async moderate(text: string) {
-    return await f<ModerationsResponse>('moderation', text);
+  async classifyModeration(text: string) {
+    return await client.createModeration(text);
   },
 };
 
